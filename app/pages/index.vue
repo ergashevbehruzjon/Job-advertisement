587295e9b56dc44506ea87f85f9bed2aa84cdced
Behruzjon <92860183+ergashevbehruzjon@users.noreply.github.com>
2023-04-10 13:32:21 +0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -23,12 +23,20 @@
               placeholder="your@email.com"
               aria-label="Email address"
             />
-
+            <input
+              ref="file"
+              class="appearance-none mb-36 bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="file"
+              name="file"
+              placeholder="Your resume"
+              @change="uploadFile"
+            />
             <button
               class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
               type="submit"
+              @click="submitFile"
             >
-              Sign Up
+              Submit
             </button>
           </form>
         </div>
