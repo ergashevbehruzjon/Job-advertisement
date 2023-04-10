84a18e0668b48b572b2a3eeeb07ed43b8aaf0db8
Behruzjon <92860183+ergashevbehruzjon@users.noreply.github.com>
2023-04-10 14:51:59 +0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -48,7 +48,7 @@
         <img
           alt="Hero"
           class="rounded shadow-xl"
-          src="../static/images/uploads/logo.png"
+          src="../static/images/uploads/teacher1.png"
         />
       </div>
     </div>
